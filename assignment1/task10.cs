55bf2b1119aee63fa59e84bde998916009d84bcf
Pradeep <131082542+Pradeepr001@users.noreply.github.com>
2024-08-14 15:20:32 +0530

    Add files via upload

---
+++ assignment1/task10.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class task10
+{
+    static void Main()
+    {
+        // Declare integer variables and assign them values
+        int a = 10;
+        int b = 20;
+        int c = 30;
+        int d = 40;
+
+        // Display each variable on a separate line
+        Console.WriteLine($"a: {a}");
+        Console.WriteLine($"b: {b}");
+        Console.WriteLine($"c: {c}");
+        Console.WriteLine($"d: {d}");
+    }
+}
---
+++ assignment1/task4.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class task4
+{
+    static void Main()
+    {
+        // Define your name
+        string name = "Pradeep";
+
+        // Use a for loop to display the name 10 times
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
